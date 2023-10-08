23951a5a1f1113e6fa8cb3ccdc04c211eaaad4c2
include9it <include9it@gmail.com>
2023-10-09 01:13:24 +0300

    project initialization

---
+++ src/main/java/io/include9it/java_url_shortener/JavaUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package io.include9it.java_url_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUrlShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/include9it/java_url_shortener/JavaUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.include9it.java_url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
